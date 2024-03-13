20a06e71a3515ec9a2474f4b4817e7f308d15697
parham <parham.mehrabi.webdev@gmail.com>
2024-03-13 23:44:21 +0330

    game's logic - guesssing game
    in this commit we parse a variable for the first time and use match keyword too
    
    we also use a loop for the first time and use continue keyword inside it

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,35 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
-
-fn main(){
-    
+fn main() {
     println!("Guess the number !!!");
     println!("Please input your guess. ");
 
-    let mut guess = String::new();
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    io::stdin()
-    .read_line(&mut guess)
-    .expect("Failed to read the line");
+    loop {
+        let mut guess = String::new();
 
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read the line");
 
-    println!("you guessed: {guess}");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("you supposed to guess a number");
+                continue;
+            }
+        };
 
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("too small"),
+            Ordering::Equal => {
+                println!("you win");
+                break;
+            }
+            Ordering::Greater => println!("too big"),
+        }
+    }
 }
