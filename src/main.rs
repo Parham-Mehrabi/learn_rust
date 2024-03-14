641a1f4c9da74aa5ab6c36fd8585808bad21446e
parham <parham.mehrabi.webdev@gmail.com>
2024-03-14 19:24:16 +0330

    variables - mutability
    checking const and let keywords and variable scopes, mutability and shadowing

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,40 @@
-use rand::Rng;
-use std::cmp::Ordering;
-use std::io;
+
+
+// const BAD_CONST = 100;       you must always annotate the type.
+// const bad_variable = 100;    you cant declare a 'let' in global scope
+const CONFIG:&str = "you can create global scoped const";
+
 
 fn main() {
-    println!("Guess the number !!!");
-    println!("Please input your guess. ");
+    const A: u16 = 60 * 10;
+    println!("A calculated in compile time: {A}");
 
-    let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    loop {
-        let mut guess = String::new();
+    let mut b: u32 = 60 * 10;
+    b += 1;
+    println!("b after increment: {b}");
 
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read the line");
 
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => {
-                println!("you supposed to guess a number");
-                continue;
-            }
-        };
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("too small"),
-            Ordering::Equal => {
-                println!("you win");
-                break;
-            }
-            Ordering::Greater => println!("too big"),
-        }
+    let x = 5;           // x is not mutable
+    let x = x + 1;      // shadowing x
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {x}");
     }
+    println!("The value of x is: {x}");
+    
+
+    println!("The config is: {CONFIG}");
+    
+    
+    
+    let spaces = "   ";
+    let spaces = spaces.len();      // you can even change the type using shadowing
+    
+    // let mut spaces2 = "   ";            // you can NOT change the type using mutation
+    // spaces2 = spaces2.len();            
+
+
+    println!("spaces: {spaces}");
+    
 }
